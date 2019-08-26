34c2a8d2afe2901db2582956367a717402131688
RaykoBzh29 <alexandre.poirel29200@gmail.com>
2019-08-26 21:15:07 +0200

    server fini

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require('./api.js');
 /*creation du server sur le port par defaut */
 const port = process.env.PORT || 3000;
 
-
 app.listen(port, () => {
   console.log(`Server is running on port: ${port} `);
 });

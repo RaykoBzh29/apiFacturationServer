59f0b17271c053cafe43b06147b1ac4bf8d9d420
RaykoBzh29 <alexandre.poirel29200@gmail.com>
2019-08-26 15:37:47 +0200

    server fini

--- db/database.js
+++ db/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'bglzhscwytizoap669gm-mysql.services.clever-cloud.com',
   port: '3306',
-  user: 'root',
-  password: '',
+  user: 'uysjg76k5ztbvfv9',
+  password: 'vD1m8JHImbGdRUh6BSfK',
   database: 'bglzhscwytizoap669gm',
 });
 module.exports = db;
